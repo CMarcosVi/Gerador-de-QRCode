4bbee86e565b098cf101f7853db463ad542a00e6
Marcos <127824847+CMarcosVi@users.noreply.github.com>
2024-12-08 20:23:15 -0300

    asd

---
+++ index.js
@@ -0,0 +1,25 @@
+function gerarQRCode() {
+    const texto = document.getElementById('inputText').value;
+    
+    if (!texto) {
+        alert("Por favor, insira um texto para gerar o QR Code.");
+        return;
+    }
+    const qrCodeImage = document.getElementById('qrCodeImage');
+    
+    QRCode.toDataURL(texto, {
+        errorCorrectionLevel: 'H',
+        type: 'image/png',
+        color: {
+            dark: '#000000',
+            light: '#0000'
+        }
+    }, (err, url) => {
+        if (err) {
+            console.error(err);
+        } else {
+            qrCodeImage.src = url;
+            qrCodeImage.style.display = "block";
+        }
+    });
+}
