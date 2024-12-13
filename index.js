7cfd22bddec4ca01ede66cbfcc125081df1925a4
Marcos <127824847+CMarcosVi@users.noreply.github.com>
2024-12-13 16:49:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-function gerarQRCode() {
-    const texto = document.getElementById('inputText').value;
-    
-    if (!texto) {
-        alert("Por favor, insira um texto para gerar o QR Code.");
-        return;
-    }
-    const qrCodeImage = document.getElementById('qrCodeImage');
-    
-    QRCode.toDataURL(texto, {
-        errorCorrectionLevel: 'H',
-        type: 'svg',
-        color: {
-            dark: '#000000',
-            light: '#0000'
+        function gerarQRCode() {
+            const texto = document.getElementById('inputText').value;
+            
+            if (!texto) {
+                alert("Por favor, insira um texto para gerar o QR Code.");
+                return;
+            }
+            
+            const qrCodeImage = document.getElementById('qrCodeImage');
+            
+            QRCode.toString(texto, {
+                errorCorrectionLevel: 'H',
+                type: 'svg', // Definindo tipo como SVG
+                color: {
+                    dark: '#000000', // cor do código (preto)
+                    light: '#0000'    // fundo transparente (sem cor)
+                }
+            }, (err, svg) => {
+                if (err) {
+                    console.error(err);
+                    alert("Erro ao gerar o QR Code. Tente novamente.");
+                } else {
+                    qrCodeImage.innerHTML = svg; // Inserindo o SVG gerado dentro do div
+                    qrCodeImage.style.display = "block"; // Exibindo o div com o SVG
+                }
+            });
         }
-    }, (err, url) => {
-        if (err) {
-            console.error(err);
-        } else {
-            qrCodeImage.innerHTML = svg;;
-            qrCodeImage.style.display = "block";
-        }
-    });
-}
