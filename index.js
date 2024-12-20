e6bd8e07e67626e2a9b679b8dab14994d0a3a58f
Marcos <127824847+CMarcosVi@users.noreply.github.com>
2024-12-20 09:35:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,65 @@
-    function gerarQRCode() {
-        const texto = document.getElementById('inputText').value;
-
-        if (!texto) {
-            alert("Por favor, insira um texto para gerar o QR Code.");
-            return;
+    document.getElementById('inputNumber').addEventListener('input', function(event) {
+        this.value = this.value.replace(/\D/g, '');
+        if (this.value.length > 11) {
+            this.value = this.value.slice(0, 11);
         }
+    });
+    document.getElementById('inputMensagem').addEventListener('input', function(event) {
+        this.value = this.value.replace(/<[^>]*>/g, '');
+        this.value = this.value.replace(/[^\w\sá-úÁ-Ú]/g, ''); 
+    });
+    function gerarLinkWpp(){
+        const numberPhone = document.getElementById('inputNumber').value;
+        const mensagemWpp = document.getElementById('inputMensagem').value;
+        const linkGenerator = document.getElementById('linkGenerator');
 
-        const qrCodeImage = document.getElementById('qrCodeImage');
-        const downloadLinkSVG = document.getElementById('downloadLinkSVG');
-        const downloadLinkPNG = document.getElementById('downloadLinkPNG');
+        if(!numberPhone){
+            return false
+        }
+        if(!mensagemWpp){
+            return false
+        }
+        linkGenerator.innerHTML = `https://api.whatsapp.com/send?phone=${numberPhone}&text=${mensagemWpp}`;
 
-        // Gerando o QR Code em SVG
-        QRCode.toString(texto, {
-            errorCorrectionLevel: 'H',
-            type: 'svg', // Definindo tipo como SVG
-            color: {
-                dark: '#000000', // cor do código (preto)
-                light: '#ffffff' // fundo branco
-            }
-        }, (err, svg) => {
-            if (err) {
-                console.error(err);
-                alert("Erro ao gerar o QR Code. Tente novamente.");
+    }
+    const btnGenertorWpp = document.getElementById("btnGenerator");
+    btnGenertorWpp.addEventListener("click", () => {
+        gerarLinkWpp()
+    })
+
+    function gerarQRCode() {
+            const texto = document.getElementById('inputText').value;
+            
+            if (!texto) {
+                alert("Por favor, insira um texto para gerar o QR Code.");
                 return;
             }
             
-            qrCodeImage.innerHTML = svg; // Inserindo o SVG gerado dentro do div
-            qrCodeImage.style.display = "block"; // Exibindo o div com o SVG
+            const qrCodeImage = document.getElementById('qrCodeImage');
+            const downloadLink = document.getElementById('downloadLink');
+            
+            QRCode.toString(texto, {
+                errorCorrectionLevel: 'H',
+                type: 'svg', // Definindo tipo como SVG
+                color: {
+                    dark: '#000000', // cor do código (preto)
+                    light: '#0000'    // fundo transparente (sem cor)
+                }
+            }, (err, svg) => {
+                if (err) {
+                    console.error(err);
+                    alert("Erro ao gerar o QR Code. Tente novamente.");
+                } else {
+                    qrCodeImage.innerHTML = svg; // Inserindo o SVG gerado dentro do div
+                    qrCodeImage.style.display = "block"; // Exibindo o div com o SVG
 
-            // Criando o Blob para o download do SVG
-            const svgBlob = new Blob([svg], { type: 'image/svg+xml' });
-            const svgUrl = URL.createObjectURL(svgBlob);
-
-            // Atualizando o link de download do SVG
-            downloadLinkSVG.href = svgUrl;
-            downloadLinkSVG.style.display = "inline"; // Exibindo o link de download do SVG
-        });
-
-        // Gerando o QR Code em PNG
-        QRCode.toDataURL(texto, {
-            errorCorrectionLevel: 'H',
-            type: 'image/png', // Definindo tipo como PNG
-            color: {
-                dark: '#000000', // cor do código (preto)
-                light: '#ffffff' // fundo branco
-            }
-        }, (err, url) => {
-            if (err) {
-                console.error(err);
-                alert("Erro ao gerar o QR Code. Tente novamente.");
-                return;
-            }
-
-            // Atualizando o link de download do PNG
-            downloadLinkPNG.href = url;
-            downloadLinkPNG.style.display = "inline"; // Exibindo o link de download do PNG
-        });
-    }
+                    // Criando o Blob para o download
+                    const blob = new Blob([svg], { type: 'image/svg+xml' });
+                    const url = URL.createObjectURL(blob);
+                    
+                    // Atualizando o link de download
+                    downloadLink.href = url;
+                    downloadLink.style.display = "inline"; // Exibindo o link de download
+                }
+            });
+        }
